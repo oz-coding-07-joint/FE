0902e7d94579d8f86053e0f5d616af0f84764d86
soohyun-bae <qosuh58@gmail.com>
2025-03-04 15:29:56 +0900

    style: 수업목록, 학습자료 컨테이너

---
+++ src/app/classroom/lecture/[lectureId]/page.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const LecturePage = () => {
+  return (
+    <div className='bg-[#F9F9F9]'>
+      <h1>title</h1>
+      <div className='bg-white rounded-md w-[390px] h-[800px] shadow-md overflow-hidden'>
+        <div className='bg-[#F5F9FF] w-[390px] h-[60px] flex justify-around items-center'>
+          <button className='h-max'>수업목록</button>
+          <div className='w-px h-[30px] bg-gray-400'></div>
+          <button className='h-max'>학습자료</button>
+        </div>
+      </div>
+    </div>
+  )
+};
+
+export default LecturePage;
