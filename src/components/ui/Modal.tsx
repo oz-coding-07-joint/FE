b047c2d5caf31e7648b9362555238d345b5df6ae
jiyea0904 <jiyea.park0904@gmail.com>
2025-03-04 17:03:16 +0900

    feat: 모달 컴포넌트 생성

---
+++ src/components/ui/Modal.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import { X } from "phosphor-react";
+import { ReactNode } from "react";
+
+type ModalProps = {
+  isOpen: boolean;
+  onClose: () => void;
+  children: ReactNode;
+};
+
+const Modal = ({ isOpen, onClose, children }: ModalProps) => {
+  if (!isOpen) return null;
+
+  return (
+    <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-40 z-50">
+      <div className="bg-white p-6 rounded-md shadow-lg w-[560px] relative">
+        <button
+          className="absolute top-3 right-3"
+          onClick={onClose}
+        >
+          <X size={30} />
+        </button>
+        {children}
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
