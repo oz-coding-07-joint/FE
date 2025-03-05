5c8e4e027128fd5b715d80d015a2b353c0a6acb5
jongho <whjk1104@gmail.com>
2025-03-05 16:48:26 +0900

    feat: 과제 관련 api

---
+++ src/api/assignmentApi.ts
@@ -0,0 +1,9 @@
+//과제 관련 API 요청 함수
+
+import axios from "axios";
+import { AssignmentList, transformAssignmentList } from "@/types/assignment";
+
+export async function fetchAssignments(lectureId: number): Promise<AssignmentList[]> {
+  const response = await axios.get(`/api/lectures/${lectureId}/assignments`);
+  return response.data.map(transformAssignmentList);
+}
