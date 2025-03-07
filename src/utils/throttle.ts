24d7f93e297c4cf96ff5c2365f94536d5aa24700
soohyun-bae <qosuh58@gmail.com>
2025-03-07 16:11:39 +0900

    fix: 빌드오류

--- src/utils/throttle.ts
+++ src/utils/throttle.ts
@@ -1,6 +1,6 @@
-type ThrottleFuction<T extends (...args: any[]) => void> = (...args: Parameters<T>) => void;
+type ThrottleFunction<T extends (...args: unknown[]) => void> = (...args: Parameters<T>) => void;
 
-export const throttle = <T extends (...args: any[]) => void>(func: T, limit: number): ThrottleFuction<T> => {
+export const throttle = <T extends (...args: unknown[]) => void>(func: T, limit: number): ThrottleFunction<T> => {
   let lastFunc: number | undefined;
   let lastRan: number | undefined;
 
