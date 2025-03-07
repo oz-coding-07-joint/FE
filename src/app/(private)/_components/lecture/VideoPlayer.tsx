f18f0c34f6f9b42df0a6ce0b7028619116337830
soohyun-bae <qosuh58@gmail.com>
2025-03-07 15:06:23 +0900

    feat: throttle, onProgress설정

--- src/app/(private)/_components/lecture/VideoPlayer.tsx
+++ src/app/(private)/_components/lecture/VideoPlayer.tsx
@@ -1,6 +1,7 @@
 import { Video } from '@/types/video';
+import { throttle } from '@/utils/throttle';
 import dynamic from 'next/dynamic';
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 
 const ReactPlayer = dynamic(() => import('react-player'), {
   ssr: false,
@@ -8,13 +9,26 @@ const ReactPlayer = dynamic(() => import('react-player'), {
 
 type VideoPlayerProps = Pick<Video, 'videoUrl'>
 
-const VideoPlayer = ({videoUrl}: VideoPlayerProps) => {
+const VideoPlayer = ({ videoUrl }: VideoPlayerProps) => {
+  const [progress, setProgress] = useState(0);
+
+  const handleProgress = useCallback(
+    throttle((state: { playedSeconds: number }) => {
+      setProgress(state.playedSeconds)
+    }, 5000), []
+  )
+
+  const handlePause = () => {
+    console.log('영상 멈춤', progress)
+  }
 
   return (
     <div className='w-[95%] aspect-video mt-5'>
       <ReactPlayer
         url={videoUrl}
         controls={true}
+        onProgress={handleProgress}
+        onPause={handlePause}
         width='100%'
         height='100%'
       />
