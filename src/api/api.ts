b3f8c8fd6de6890fc6170c4be261ec543aad5161
jiyea0904 <jiyea.park0904@gmail.com>
2025-03-11 13:05:33 +0900

    fix: api 주소 변경

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://223.130.136.197/api/v1",
+  baseURL: "http://211.188.59.23/api/v1",
   withCredentials: true, // 쿠키 자동 전송
   headers: {
     "Content-Type": "application/json",
