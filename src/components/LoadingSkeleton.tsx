1ebec4f6b0282574445fda0c8073ca247e102979
soohyun-bae <qosuh58@gmail.com>
2025-03-13 12:00:15 +0900

    style: loading:

---
+++ src/components/LoadingSkeleton.tsx
@@ -0,0 +1,11 @@
+
+const LoadingSkeleton = () => {
+  return (
+    <div className='flex animate-pulse'>
+      <div className='bg-muted-200'>
+      </div>
+    </div>
+  );
+};
+
+export default LoadingSkeleton;
