497f8712cbaa590ceea74f1ab359655e8f2e14c2
soohyun-bae <qosuh58@gmail.com>
2025-03-13 16:04:01 +0900

    feat: lecture link추가

--- src/app/(private)/classroom/lecture/page.tsx
+++ src/app/(private)/classroom/lecture/page.tsx
@@ -4,6 +4,7 @@
 import { useEffect, useState } from "react";
 import { fetchLectures } from "@/api/lectureApi";
 import { Lecture } from "@/types/class";
+import Link from "next/link";
 
 export default function LecturePage() {
   const [lectures, setLectures] = useState<Lecture[]>([]);
@@ -31,38 +32,40 @@ export default function LecturePage() {
       <h1 className="text-2xl font-bold text-gray-800 mb-6">강의 목록</h1>
       <div className="grid grid-cols-[repeat(auto-fit,minmax(250px,300px))] gap-4 justify-items-center">
         {lectures.map((lecture) => (
-          <div
-            key={lecture.id}
-            className="bg-white rounded-lg shadow-md w-full min-w-[250px] max-w-[300px]"
-          >
-            <img
-              src={lecture.thumbnailUrl || "@/assets/images/no-img.png"} 
-              alt={lecture.title}
-              className="w-full h-48 object-cover rounded-t-lg"
-            />
-            <div className="p-4">
-              <h3 className="text-lg font-bold text-gray-800 mb-2">
-                {lecture.title}
-              </h3>
-              <div className="w-full bg-gray-200 h-2 rounded-full">
-                <div
-                  className="bg-primary-600 h-2 rounded-full"
-                  style={{ width: `${lecture.progressRate}%` }}
-                ></div>
-              </div>
-              <p className="text-gray-500 text-sm mt-1">
-                {lecture.progressRate}% 강좌 완료
-              </p>
-              <div className="flex mt-4">
-                <button className="bg-white border border-gray-300 text-gray-600 px-3 py-1 rounded flex-1">
-                  수업 정보 보기
-                </button>
-                <button className="bg-gray-600 text-white px-3 py-1 rounded hover:bg-gray-700 flex-1 ml-3">
-                  수업 후기 작성
-                </button>
+          <Link key={lecture.id} href={`/classroom/lecture/${lecture.id}`}>
+            <div
+              key={lecture.id}
+              className="bg-white rounded-lg shadow-md w-full min-w-[250px] max-w-[300px]"
+            >
+              <img
+                src={lecture.thumbnailUrl || "@/assets/images/no-img.png"}
+                alt={lecture.title}
+                className="w-full h-48 object-cover rounded-t-lg"
+              />
+              <div className="p-4">
+                <h3 className="text-lg font-bold text-gray-800 mb-2">
+                  {lecture.title}
+                </h3>
+                <div className="w-full bg-gray-200 h-2 rounded-full">
+                  <div
+                    className="bg-primary-600 h-2 rounded-full"
+                    style={{ width: `${lecture.progressRate}%` }}
+                  ></div>
+                </div>
+                <p className="text-gray-500 text-sm mt-1">
+                  {lecture.progressRate}% 강좌 완료
+                </p>
+                <div className="flex mt-4">
+                  <button className="bg-white border border-gray-300 text-gray-600 px-3 py-1 rounded flex-1">
+                    수업 정보 보기
+                  </button>
+                  <button className="bg-gray-600 text-white px-3 py-1 rounded hover:bg-gray-700 flex-1 ml-3">
+                    수업 후기 작성
+                  </button>
+                </div>
               </div>
             </div>
-          </div>
+          </Link>
         ))}
       </div>
     </div>
