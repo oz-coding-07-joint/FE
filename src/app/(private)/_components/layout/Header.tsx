13e8ca632a9a5c0d010e3000134ce8bd85f2eb2b
jiyea0904 <jiyea.park0904@gmail.com>
2025-03-13 11:44:18 +0900

    feat: 로그아웃 api 연결

--- src/app/(private)/_components/layout/Header.tsx
+++ src/app/(private)/_components/layout/Header.tsx
@@ -1,13 +1,12 @@
 'use client';
-import { UserCircle } from 'phosphor-react';
+import UserMenu from '@/components/UserMenu';
 import React from 'react';
 
 const Header = () => {
   return (
     <header className='w-full h-16 bg-white shadow-md'>
-      <div className='flex items-center justify-end gap-1 h-full'>
-        <UserCircle size={30} weight="fill" />
-        <h3 className='pr-5 text-lg'>홍길동 님</h3>
+      <div className='flex items-center justify-end gap-1 h-full pr-10'>
+        <UserMenu />
       </div>
     </header>
   );
--- src/store/useAuthStore.ts
+++ src/store/useAuthStore.ts
@@ -1,23 +1,41 @@
 import { create } from "zustand";
 import { User } from "@/types/auth";
+import { getUserInfo } from "@/api/authApi";
 
 interface AuthState {
   user: User | null;
   login: (user: User) => void;
   logout: () => void;
-  fetchUserInfo: (user: User) => void;
+  restoreUser: () => Promise<void>;
+  updateUser: (updatedUser: User) => void;
 }
 
-export const useAuthStore = create<AuthState>((set) => ({
+export const useAuthStore = create<AuthState>()((set) => ({
   user: null,
+
   login: (user) => {
-    console.log("로그인 상태 업데이트", user);
-    set(() => ({ user }));
-  },
-  logout: () => set(() => ({ user: null })),
-  fetchUserInfo: (user) => {
-    console.log("유저 정보 갱신", user);
+    console.log("로그인 성공:", user);
     set(() => ({ user }));
   },
 
+  logout: () => {
+    console.log("🚪 로그아웃 완료");
+    set(() => ({ user: null }));
+  },
+
+  restoreUser: async () => {
+    try {
+      const user = await getUserInfo();
+      console.log("새로고침 후 유저 정보 복원:", user);
+      set(() => ({ user }));
+    } catch (error) {
+      console.error("유저 정보 복원 실패:", error);
+      set(() => ({ user: null }));
+    }
+  },
+
+  updateUser: (updatedUser) => {
+    console.log("유저 정보 업데이트:", updatedUser);
+    set(() => ({ user: updatedUser })); // 상태 업데이트
+  },
 }));
