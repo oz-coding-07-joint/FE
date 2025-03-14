2dd6527cf750e164ade0a70e05729c9f6fb1dcde
MyangSZ <unoyou@naver.com>
2025-03-14 16:30:43 +0900

    feat: 랜딩페이지 클래스섹션(교육과정안내 수정)

--- src/app/(public)/_components/Class.tsx
+++ src/app/(public)/_components/Class.tsx
@@ -3,10 +3,40 @@ import Image from "next/image";
 import Classimg from "../../../assets/images/classimg.jpg";
 import { useState } from "react";
 import { ChevronDownIcon } from "@heroicons/react/24/solid";
+// import VideoPlayer from "@/app/(private)/_components/lecture/VideoPlayer";
 
 export default function Class() {
-  const [selectedCourse, setSelectedCourse] = useState<string>("화성학의 기초");
-  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const [openIndex, setOpenIndex] = useState<number | null>(null);
+  const toggleAccordion = (index: number) => {
+    setOpenIndex(openIndex === index ? null : index);
+  };
+
+  const courseData = [
+    {
+      title: "화성학의 기초",
+      lectures: [
+        "01. 동영상 강의명이 들어갑니다.",
+        "02. 동영상 강의명이 들어갑니다.",
+        "03. 동영상 강의명이 들어갑니다.",
+      ],
+    },
+    {
+      title: "화성학의 기초2",
+      lectures: [
+        "01. 동영상 강의명이 들어갑니다.",
+        "02. 동영상 강의명이 들어갑니다.",
+        "03. 동영상 강의명이 들어갑니다.",
+      ],
+    },
+    {
+      title: "화성학 실습",
+      lectures: [],
+    },
+    {
+      title: "화성학 실습",
+      lectures: [],
+    },
+  ];
 
   return (
     <div className="h-[1080px] flex flex-col justify-center items-center gap-5">
@@ -20,84 +50,69 @@ export default function Class() {
           들어갑니다.
         </span>
       </div>
-      <div className="flex gap-10 w-[700px] lg:w-[1080px]">
+      <div className="flex justify-center gap-10 w-[700px] lg:w-[1080px]">
         <div className="flex flex-col justify-start gap-8">
+          {/* <div className="w-[400px] lg:w-[550px] h-[400px] rounded-lg">
+            <VideoPlayer
+              videoUrl={"https://youtu.be/GnvztDUhH3w"}
+              chapterVideoId={null}
+            />
+          </div> */}
           <Image
             src={Classimg}
             alt="Logo"
             className="w-[400px] lg:w-[660px] h-[400px] flex items-start rounded-lg object-cover object-top"
           />
-          <span className="text-white font-normal lg:text-xl text-lg">
-            <li>수강기간 : </li>
-            <li>수강인원 : </li>
-            <li>강사 : </li>
-          </span>
+          <div>
+            <ul className="text-white font-normal lg:text-xl text-lg list-disc list-inside">
+              <li>수강기간 : </li>
+              <li>수강인원 : </li>
+              <li>강사 : </li>
+            </ul>
+          </div>
         </div>
-        <div className="w-[500px] text-white flex flex-col">
-          <span>교육과정 안내</span>
-          <div className="relative w=[350px]">
-            <button
-              onClick={() => setIsOpen(!isOpen)}
-              className="w-full h-[50px] text-muted-400 pl-5 pr-10 rounded-md border border-white bg-white text-left flex justify-between items-center font-semibold"
-            >
-              {selectedCourse}
-              <ChevronDownIcon
-                className={`w-6 h-6 text-gray-700 transform transition-transform ${
-                  isOpen ? "rotate-180" : "rotate-0"
-                }`}
-              />
-            </button>
-            {/* 드롭다운 리스트 */}
-            {isOpen && (
-              <ul
-                className="absolute left-0 mt-1 w-full bg-white border border-gray-300
-              rounded-md shadow-lg overflow-hidden transition-opacity opacity-100 z-10 text-muted-400 font-semibold"
+
+        {/* 교육과정 */}
+        <div className="w-[500px] text-white flex flex-col gap-2 ">
+          <span className="text-2xl font-normal mb-3">교육과정 안내</span>
+
+          {/* 목록   */}
+          <div className="w-full text-white flex flex-col gap-2 font-normal text-lg items-center">
+            <div className="h-[350px] mb-3 flex  flex-col gap-1">
+              {courseData.map((course, index) => (
+                <div key={index} className="relative w-[350px] lg:w-[500px]">
+                  <button
+                    onClick={() => toggleAccordion(index)}
+                    className="w-full h-[50px] px-5 rounded-sm border border-white text-white font-normal text-left flex justify-between items-center"
+                  >
+                    {course.title}
+                    <ChevronDownIcon
+                      className={`w-6 h-6 text-white transform transition-transform ${
+                        openIndex === index ? "rotate-180" : "rotate-0"
+                      }`}
+                    />
+                  </button>
+                  {openIndex === index && course.lectures.length > 0 && (
+                    <div className="border border-white p-4 mt-1 rounded-sm">
+                      {course.lectures.map((lecture, idx) => (
+                        <p key={idx} className="leading-7">
+                          {lecture}
+                        </p>
+                      ))}
+                    </div>
+                  )}
+                </div>
+              ))}
+            </div>
+
+            <div className="flex justify-center items-center h-24">
+              <button
+                className="text-center w-[350px] lg:w-[500px] h-14 bg-white
+              rounded-sm bg-yellow text-muted-600 font-semibold cursor-pointer"
               >
-                <li
-                  className="px-5 py-3 hover:bg-gray-100 cursor-pointer"
-                  onClick={() => {
-                    setSelectedCourse("화성학의 기초");
-                    setIsOpen(false);
-                  }}
-                >
-                  화성학의 기초
-                </li>
-                <li
-                  className="px-5 py-3 hover:bg-gray-100 cursor-pointer"
-                  onClick={() => {
-                    setSelectedCourse("고급 화성학");
-                    setIsOpen(false);
-                  }}
-                >
-                  고급 화성학
-                </li>
-              </ul>
-            )}
-          </div>
-
-          {/* <select
-              name="class-name"
-              value={selectedCourse}
-              onChange={(e) => setSelectedCourse(e.target.value)}
-              className="w-[350px] lg:w-[500px] h-[50px] text-[#666666] pl-5 rounded-lg border-white bg-none appearance-none after:content-['▼'] after:absolute after:right-5"
-            >
-              <option value="화성학의 기초">화성학의 기초</option>
-              <option value="고급 화성학">고급 화성학</option>
-            </select> */}
-          {/* Heroicons 화살표 아이콘 */}
-          {/* <ChevronDownIcon className="absolute right-4 top-1/2 w-6 h-6 text-black transform -translate-y-1/2 pointer-events-none" />
-          </div> */}
-
-          <div className="border border-solid font-thin w-[350px] lg:w-[500px] h-52 mt-8 p-4">
-            <span className="font-medium">선택된 과정 : {selectedCourse}</span>
-          </div>
-          <div className="mt-8 flex justify-center">
-            <button
-              className="text-center w-[350px] lg:w-[500px] h-12 bg-white
-              rounded-md bg-yellow text-black hover:bg-primary-800 hover:text-white"
-            >
-              강의 자세히 보러가기
-            </button>
+                강의 자세히 보러가기
+              </button>
+            </div>
           </div>
         </div>
       </div>
