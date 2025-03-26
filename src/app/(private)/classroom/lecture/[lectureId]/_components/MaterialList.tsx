b39aa5cc35ded8fb0635eb67166499773d409dff
soohyun-bae <qosuh58@gmail.com>
2025-03-26 11:44:38 +0900

    fix: file download

--- src/app/(private)/classroom/lecture/[lectureId]/_components/MaterialList.tsx
+++ src/app/(private)/classroom/lecture/[lectureId]/_components/MaterialList.tsx
@@ -1,27 +1,55 @@
 import { Chapter } from '@/types/video';
 import { FilePdf } from 'phosphor-react';
+import { useState } from 'react';
 
-type MaterialListProps = Partial<Pick<Chapter, 'materialInfo'>>
+type MaterialListProps = Partial<Pick<Chapter, 'materialInfo'>> 
 
 const MaterialList = ({ materialInfo }: MaterialListProps) => {
+  const [blobUrl, setBlobUrl] = useState<string | null>(null)
+  const [downloading, setDownloading] = useState(false);
+
   if(!materialInfo) return null;
   console.log("Download URL:", materialInfo.downloadUrl);
+  console.log("Download Name:", materialInfo.fileName);
 
-  const fileName = materialInfo.fileName.replace(/^materials_([^_]+_[^_]+)_\S+\.\w+$/, "$1");
+  const handleDownload = async () => {
+    if(!materialInfo.downloadUrl) {
+      console.error ('Download URL is missing');
+      return;
+    }
+    setDownloading(true);
+    try {
+      const response = await fetch(materialInfo.downloadUrl);
+      if(!response.ok) throw new Error('Failed to fetch file');
+
+      const blob = await response.blob();
+      const url = window.URL.createObjectURL(blob);
+      setBlobUrl(url)
+
+      const a = document.createElement('a');
+      a.href = url;
+      a.download = materialInfo.fileName;
+      document.body.appendChild(a);
+      a.click();
+      document.body.removeChild(a);
+
+    } catch (error) {
+      console.error('Download failed:', error);
+    } finally {
+      setDownloading(false);
+    }
+  }
 
   return (
     <div className='w-[85%]'>
-      <a className='flex items-center gap-3'
-        href={materialInfo.downloadUrl}
-        download={materialInfo.downloadUrl}
-        target='_blank'
-        rel="noopener noreferrer"
+      <button className='flex items-center gap-3'
+        onClick={handleDownload}
       >
         <FilePdf size={16} />
         <p className='w-[200px] line-clamp-1'>
-          {fileName}
+          {materialInfo.fileName}
         </p>
-      </a>
+      </button>
     </div>
   );
 };
