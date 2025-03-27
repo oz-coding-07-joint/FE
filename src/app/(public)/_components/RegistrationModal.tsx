b683ba2a91d68d8303f39f8e71e80147b3fe0f38
MyangSZ <unoyou@naver.com>
2025-03-27 16:04:24 +0900

    chore: footer 수정. 수강신청모달 내용수정

--- src/app/(public)/_components/RegistrationModal.tsx
+++ src/app/(public)/_components/RegistrationModal.tsx
@@ -9,25 +9,31 @@ export default function RegistrationModal() {
 
   return (
     <Modal modalKey="registration">
-      <div className="flex flex-col p-6 gap-5 items-center text-muted-600">
+      <div className="flex flex-col py-6 gap-5 items-center text-muted-600">
         <h2 className="text-4xl">수강신청</h2>
         <span>
           수강신청을 진행하기 위해선 아래 계좌로 이체를 진행해주세요
           <br /> 이체 확인 후 수강등록이 완료됩니다.
         </span>
-
-        <div className="bg-muted-200 list-none flex flex-col items-center px-20 py-5 bg-opacity-40 text-muted-600">
+        {/* 계좌정보 */}
+        <div className="bg-muted-200 list-none flex flex-col items-center px-28 py-6 mb-2 bg-opacity-40 text-muted-600">
           <li>카카오뱅크 : 3333-111111-111111</li>
           <li>계좌명 : 소리상상</li>
           <li>결제금액 : 330,000원</li>
         </div>
-        <div className="flex flex-col justify-start w-96 p-6">
+        <div className="flex flex-col justify-start p-6 border-t">
           <span className="font-medium text-lg mb-2">주의사항</span>
-          <ul className="list-disc list-inside text-muted-400">
-            <li>주의사항내용</li>
-            <li>주의사항내용</li>
-            <li>주의사항내용</li>
-          </ul>
+          <div>
+            <ul className="list-decimal list-outside text-muted-400 text-sm whitespace-pre-line pl-4">
+              <li>수강생명과 동일한 명의로 이체 바랍니다.</li>
+              <li>수취인명을 반드시 확인 후 이체 바랍니다.</li>
+              <li>이체 후 확인까지 시간이 걸릴 수 있는 점 양해부탁드립니다.</li>
+              <li>
+                {`기타 시스템오류로 인한 입금 확인 불가건의 경우
+              고개센터로 문의해주세요.`}
+              </li>
+            </ul>
+          </div>
         </div>
         <Button
           label="닫기"
--- src/app/(public)/_components/layout/Footer.tsx
+++ src/app/(public)/_components/layout/Footer.tsx
@@ -1,39 +1,38 @@
 export default function Footer() {
   return (
     <div>
-      <div className="h-[180px] py-5 text-white font-normal bg-[#121C30] text-sm px-10 flex flex-col gap-6">
+      <div className=" py-5 text-white font-normal bg-[#121C30] text-sm px-10 flex flex-col gap-6">
         <div className="flex flex-col leading-6">
           <div>
-            <span className="font-semibold">사업자명</span>
+            <span className="font-semibold">상호명</span>
             <span> : 음악더하기</span>
-            <span className="font-semibold">
-              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;대표자
-            </span>
-            <span> : 김진성</span>
+            <div>
+              <span className="font-semibold">사업자등록번호</span>
+              <span>&nbsp;: 450-20-01366</span>
+            </div>
+            <div>
+              <span className="font-semibold">대표</span>
+              <span> : 김진성</span>
+            </div>
           </div>
           <div>
-            <span className="font-semibold">사업장주소</span>
+            <span className="font-semibold">주소</span>
             <span> : 서울 광진구 천호대로118길 50 2층</span>
-            <span className="font-semibold">
-              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;대표번호
-            </span>
-            <span> : 0507-1352-4682</span>
-
-            <span className="font-semibold">
-              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;이메일
-            </span>
-            <span> : jinseongkim@addmusicplus.com</span>
+            <div>
+              <span className="font-semibold">대표번호</span>
+              <span> : 0507-1352-4682</span>
+            </div>
+            <div>
+              <span className="font-semibold">이메일</span>
+              <span> : jinseongkim@addmusicplus.com</span>
+            </div>
           </div>
           <div className="flex flex-row">
-            <span className="font-semibold">사업자등록번호</span>
-            <span>&nbsp;: 450-20-01366</span>
-            <span className="font-semibold">
-              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;통신판매업자번호
-            </span>
+            <span className="font-semibold">통신판매업자번호</span>
             <span>&nbsp;: (준비중)</span>
           </div>
         </div>
-        <div>COPYRIGHT @2025 소리상상</div>
+        <div>COPYRIGHT ⓒ2025 소리상상</div>
       </div>
     </div>
   );
